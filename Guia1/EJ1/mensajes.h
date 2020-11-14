5671f1c8aa1acc91e04bfd0590bd2a0f9a096c53
SledgeHammer <jsuarezbaronbc@gmail.com>
2020-11-14 00:41:01 +0000

    Ej1

---
+++ Guia1/EJ1/mensajes.h
@@ -0,0 +1,15 @@
+#define CONV_DOLAR_EURO 0.85
+#define CONV_DOLAR_PESO 181.0
+#define CONV_DOLAR_REAL 5.56
+#define CASO_DOLAR 1
+#define CASO_EURO 2
+#define CASO_PESO 3
+#define CASO_REAL 4
+#define MSJ_CANT_DINERO "Ingrese la cantidad de dinero que desea transformar"
+#define MSJ_SELECCION_DIVISA "Para seleccionar la divisa que desea convertir va a necesitar ingresar:\n1, Por si usted quiere transformar dolares.\n2,Por si usted quiere transformar Euros.\n3,Por si usted quiere transformar Pesos.\n4,Por si usted quiere transformar Reales."
+#define MSJ_GRACIAS "Gracias por usar nuestro servicio"
+#define MSJ_CANTIDAD_PESOS "La cantidad de Pesos es:"
+#define MSJ_CANTIDAD_EUROS "La cantidad de Euros es:"
+#define MSJ_CANTIDAD_REALES "La cantidad de Reales es:"
+#define MSJ_CANTIDAD_DOLARES "La cantidad de Dolares es:"
+#define MSJ_ERROR_VALOR "Ingreso un valor erroneo."
